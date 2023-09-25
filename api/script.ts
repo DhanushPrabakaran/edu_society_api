472b10e92551ed094cf66e12271567ebfa2acc55
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-09-25 20:15:02 +0530

    update

--- api/script.ts
+++ api/script.ts
@@ -21,3 +21,23 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+
+// Import the express in typescript file
+import express from 'express';
+
+// Initialize the express engine
+const app: express.Application = express();
+
+// Take a port 3000 for running server.
+const port: number = 3000;
+
+// Handling '/' Request
+app.get('/', (_req, _res) => {
+	_res.send("TypeScript With Express");
+});
+
+// Server setup
+app.listen(port, () => {
+	console.log(`TypeScript with Express
+		http://localhost:${port}/`);
+});
